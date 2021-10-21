ee75ca88f0e4ba90ecf53a27c9553b0f53bfc004
Alejandro Lozano <alejandro.lozano@nxp.com>
2021-10-21 17:20:20 -0500

    ADA: Beta Release
    Signed-off-by: Alejandro Lozano <alejandro.lozano@nxp.com>

--- backgroundRemoval.cc
+++ backgroundRemoval.cc
@@ -36,7 +36,7 @@ int main(int argc, char * argv[]) {
         if (!cam.isOpened()) return -1;
         /*Save JPG of first capture*/
         cam >> colorMat;
-        imwrite("Prueba.jpg", colorMat);
+        //imwrite("Prueba.jpg", colorMat);
 
         cv::Size s = colorMat.size();
         uint32_t imageSize = s.height * s.width;
@@ -44,21 +44,9 @@ int main(int argc, char * argv[]) {
         uint32_t * ranks = new uint32_t[imageSize];
         uint32_t nEdges = imageSize * 2 - (s.width + s.height);
         uint32_t * edges = new uint32_t[nEdges *3];
-        uint32_t sortedEdges[nEdges][3] = {}; 
-
-        auto start = high_resolution_clock::now();
-        initParents(parents, imageSize);
-        initRanks(ranks, imageSize);
-        *parents = *parents++;
-        *ranks = *ranks++;
-        auto max = buildEdges(colorMat, (uint32_t(*)[3])edges, 0);
-        sortEdges((uint32_t(*)[3])edges, sortedEdges, nEdges, max);
-        auto stop = high_resolution_clock::now();
-
-        //printEdgeArray(sortedEdges, imageSize * 2 - (s.width + s.height));
-        auto duration = duration_cast<microseconds>(stop - start);
-        cout << "buildEdges: " << duration.count() << " Microseconds"<< endl;
-
+        uint32_t * sortedEdges= new uint32_t[nEdges *3]; 
+        uint32_t threshold = 6;
+        uint32_t mergedCount, maxLeader;
 
 /*****************************Build Edges Test****************************************/
 
@@ -66,8 +54,28 @@ int main(int argc, char * argv[]) {
 
         for(;;) {
                 cam >> colorMat;
-                imshow("Color",colorMat);
                 
+                auto start = high_resolution_clock::now();
+                initParentsAndRanks(parents, ranks, imageSize);
+                auto max = buildEdges(colorMat, (uint32_t(*)[3])edges, 0);
+                sortEdges((uint32_t(*)[3])edges, (uint32_t(*)[3])sortedEdges, nEdges, max);
+                //printEdgeArray(sortedEdges, imageSize * 2 - (s.width + s.height));
+                mergedCount = kruskal((uint32_t(*)[3])sortedEdges, nEdges, imageSize, parents, ranks, threshold);
+                maxLeader = getMaxParent(ranks, imageSize);
+                RGB * p = colorMat.ptr<RGB>(0);
+                for (int i = 0 ; i < imageSize; i++, p++) {
+                        uint32_t l = findLeader(parents, imageSize, i);
+                        if (l == maxLeader) {
+                                p->blue = 0;
+                                p->green = 0;
+                                p->red = 0;
+                        }
+                }
+                auto stop = high_resolution_clock::now();
+                auto duration = duration_cast<microseconds>(stop - start);
+                cout << duration.count() << " Microseconds " << "MergedCount " << mergedCount << " MaxLeader " <<  maxLeader << endl;    
+                imshow("Color",colorMat);
+
                 char c=(char)waitKey(25);
                 if(c==27)
                         break;
