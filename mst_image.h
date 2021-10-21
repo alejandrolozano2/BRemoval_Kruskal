a3f00b7e9c2934e8a9e3503e4b0a4b06a7da50ae
Alejandro Lozano <alejandro.lozano@nxp.com>
2021-10-21 12:00:45 -0500

    ADA: inline init parents and ranks
    Used by Kruskal
    
    Signed-off-by: Alejandro Lozano <alejandro.lozano@nxp.com>

--- mst_image.h
+++ mst_image.h
@@ -10,6 +10,7 @@
 #include <time.h>
 #include <cstdlib>
 
+
 /*Finds parent. Used for find/joing algorithm*/
 uint32_t findLeader(uint32_t * parents, uint32_t size, uint32_t value);
 
@@ -18,5 +19,17 @@ bool join(uint32_t * parents, uint32_t * rank, uint32_t size, uint32_t i, uint32
 
 /*MST Kruskal i,j,w*/
 void kruskal(uint32_t  (* edges)[3], uint32_t size);
+
+
+static inline  __attribute__((always_inline)) void initParents(uint32_t * parents, uint32_t len) {
+    for (uint32_t i = 0; i < len; i++) {
+        *parents++ = i;
+    }
+}
+static inline __attribute__((always_inline)) void initRanks(uint32_t * ranks, uint32_t len) {
+    for (uint32_t i = 0; i < len; i++) {
+        *ranks++ = 1;
+    }
+}
 
 #endif
